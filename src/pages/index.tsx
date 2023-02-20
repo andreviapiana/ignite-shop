893c616c73c1835324578417c6587ecec8e9e175
André Viapiana <andreviapiana@gmail.com>
2023-02-20 20:41:39 -0300

    ✨feat: Criação do Carousel com Setas (Keen Slider)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,58 @@
 import Image from 'next/image'
-import { HomeContainer, Product } from '../styles/pages/home'
+
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
+import { ArrowButton, HomeContainer, Product } from '../styles/pages/home'
 
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
+import { useState } from 'react'
 
 export default function Home() {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
+
+  function Arrow(props: any) {
+    // const disabled = props.disabled ? arrowDisabled() : "";
+    return (
+      <ArrowButton
+        direction={props.left ? 'left' : 'right'}
+        disabled={props.disabled}
+      >
+        <svg
+          onClick={props.onClick}
+          width="48"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          {props.left && (
+            <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+          )}
+          {!props.left && (
+            <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+          )}
+        </svg>
+      </ArrowButton>
+    )
+  }
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
@@ -17,7 +61,7 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
 
         <footer>
@@ -25,6 +69,46 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      {loaded && instanceRef.current && (
+        <>
+          <Arrow
+            left
+            onClick={(e: { stopPropagation: () => any }) =>
+              e.stopPropagation() || instanceRef.current?.prev()
+            }
+            disabled={currentSlide === 0}
+          />
+
+          <Arrow
+            onClick={(e: { stopPropagation: () => any }) =>
+              e.stopPropagation() || instanceRef.current?.next()
+            }
+            disabled={
+              currentSlide ===
+              instanceRef.current.track.details.slides.length - 1
+            }
+          />
+        </>
+      )}
     </HomeContainer>
   )
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from '..'
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
@@ -12,7 +11,6 @@ export const HomeContainer = styled('main', {
 export const Product = styled('a', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
@@ -59,6 +57,42 @@ export const Product = styled('a', {
     footer: {
       transform: 'translateY(0%)',
       opacity: 1,
+    },
+  },
+})
+
+export const ArrowButton = styled('button', {
+  position: 'absolute',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  width: '8.5rem',
+  height: '100%',
+  background:
+    'linear-gradient(270deg, rgba(18, 18, 20, 0) 0%, rgba(18, 18, 20, 0.75) 100%)',
+  cursor: 'pointer',
+  border: 'none',
+  outline: 'none',
+  color: '$white',
+  fill: '$white',
+  padding: '0 1rem',
+
+  variants: {
+    direction: {
+      left: {
+        left: 0,
+        textAlign: 'left',
+      },
+      right: {
+        right: 0,
+        textAlign: 'right',
+        background:
+          'linear-gradient(90deg, rgba(18, 18, 20, 0) 0%, rgba(18, 18, 20, 0.75) 100%)',
+      },
+    },
+    disabled: {
+      true: {
+        opacity: 0.5,
+      },
     },
   },
 })
